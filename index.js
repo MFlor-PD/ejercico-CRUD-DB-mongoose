64c98f36e54804dec81cb999a332f768c98c0e43
Flor <mariaflorenciaperezd@gmail.com>
2025-08-08 10:35:42 +0200

    Auth to database fixed + message in Home

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const { dbConnection } = require('./config/config');
 const Taskroutes = require('./routes/tasks.js');
 
 app.use(express.json());
+app.get('/', (req, res) =>{
+    res.status(201).send('Agregar /tasks en el local host para traer resultados de MongoDb');
+})
 app.use("/tasks", Taskroutes);
 
 //console.log("MONGO_URI desde index.js:", process.env.MONGO_URI); // <-- Añade esta línea
 dbConnection();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port: http://localhost:${PORT}`));
